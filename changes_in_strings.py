05b42d91eb06e092036b8f7de0cfabcf0f5b260b
manish8826 <107915498+manish8826@users.noreply.github.com>
2022-07-13 13:32:48 +0530

    Add files via upload

---
+++ changes_in_strings.py
@@ -0,0 +1,17 @@
+a = "manish"
+
+print(a.upper())  #upper case
+
+print(a.replace("manish", "heena")) #replacing string
+
+print(a.find("i")) #finding the index of particular letter in sring
+
+print(a.find("manish")) #where indexing starts , Note: indexing always starts from zero(0)
+
+print(a.replace("m", "M"))
+
+print(a.find("d")) #if that letter or string not there in the defined variable's value then output is -1 always
+
+print("m" in a)# "in" is a keyword which is already defined in python if the character or the string exists in the defined string then it gives "TRUE" boolean ortherwise "FALSE" boolean
+
+print("z" in a)
---
+++ practice.py
@@ -0,0 +1,33 @@
+#Question-"define variables name given Tony Stark, Age =
+# 51, and Tony is genius (with the help of boolean) 
+
+
+FirstName = "Tony"
+LastName = "Stark"
+
+print(FirstName,LastName)
+
+Age = 51
+
+Tony_is_a_genius = (True)
+print(Tony_is_a_genius)
+#Question- Tony is secretly a superhero ask him for his superhero name & show it to the terminal
+
+superhero = input('what is your superhero name? ')
+print(superhero)
+
+num1 = input("enter first digit ")
+num2 = input("enter second digit ")
+
+sum = int(num1) +int(num2)
+print(sum)
+
+#QUESTION- TAKE 4 INPUT AND MULTIPLY THEM
+
+digit = input("enter num 1 ")
+digit2 = input("enter num 2 ")
+
+product = int(digit) * int(digit2)
+
+print(product)
+
